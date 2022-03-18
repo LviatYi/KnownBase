528c01c5d1708acdede9f6aec118de22a8e1490d
Lviat Yi <LviatYi@qq.com>
2022-03-18 15:03:27 +0800

    Update Solution21.h

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution21.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution21 {
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
+};
+

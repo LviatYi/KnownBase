5b1d544c39d1798f5b2cf5c795153812d1ee8dfd
Lviat Yi <LviatYi@qq.com>
2022-03-19 22:02:15 +0800

    Add public ListNode class

--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution19.h
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution19.h
@@ -1,13 +1,5 @@
 #pragma once
-
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
-
-};
+#include "ListNode.h"
 
 class Solution19 {
 public:
--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution2.h
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution2.h
@@ -1,14 +1,5 @@
 #pragma once
-//Definition for singly-linked list.
-//struct ListNode;
-
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
-};
+#include "ListNode.h"
 
 class Solution2 {
 public:

491502b514919cd735a40ca712a94a656b3ae963
Lviat Yi <LviatYi@qq.com>
2022-03-19 22:08:33 +0800

    Update solution23

--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution23.cpp
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution23.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+#pragma region 优先队列
 // 自定义类型
 // 若使用 greater 仿函数则需重载 >
 // 若使用 less 仿函数则需重载 <
@@ -24,8 +25,6 @@ struct ListNodeComp {
 };
 
 ListNode* Solution23::mergeKLists(std::vector<ListNode*>& lists) {
-    // 优先队列
-
     // 优先队列
     // 元素类型，内部容器（默认为 vector<T>），比较类（仿函数）（默认为 less<T>）
     priority_queue<ListNodeComp, vector<ListNodeComp>, greater<ListNodeComp>> queue;
@@ -48,3 +47,33 @@ ListNode* Solution23::mergeKLists(std::vector<ListNode*>& lists) {
 
     return preAnsHead->next;
 }
+#pragma endregion
+
+#pragma region 二分合并
+ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+    if ((!a) || (!b)) return a ? a : b;
+    ListNode head, * tail = &head, * aPtr = a, * bPtr = b;
+    while (aPtr && bPtr) {
+        if (aPtr->val < bPtr->val) {
+            tail->next = aPtr; aPtr = aPtr->next;
+        }
+        else {
+            tail->next = bPtr; bPtr = bPtr->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = (aPtr ? aPtr : bPtr);
+    return head.next;
+}
+
+ListNode* merge(vector <ListNode*>& lists, int l, int r) {
+    if (l == r) return lists[l];
+    if (l > r) return nullptr;
+    int mid = (l + r) >> 1;
+    return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+}
+
+ListNode* Solution23::mergeKLists2(vector<ListNode*>& lists) {
+    return merge(lists, 0, lists.size() - 1);
+}
+#pragma endregion
--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution23.h
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution23.h
@@ -6,4 +6,5 @@
 class Solution23 {
 public:
     ListNode* mergeKLists(std::vector<ListNode*>& lists);
+    ListNode* mergeKLists2(std::vector<ListNode*>& lists);
 };

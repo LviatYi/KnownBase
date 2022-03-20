931d6bddf02e7a7330ddcdf9120c668634ec8db2
Lviat Yi <LviatYi@qq.com>
2022-03-20 16:37:36 +0800

    Update Solution29.cpp

--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution29.cpp
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution29.cpp
@@ -1,14 +1,42 @@
 #include "Solution29.h"
-#include <math.h>
-int Solution29::divide(int dividend, int divisor) {
-    int flag = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
-    long long _dividend = dividend > 0 ? long long(dividend) : long long(-dividend);
-    long long _divisor = divisor > 0 ? long long(divisor) : long long(-divisor);
+#include <limits>
 
-    int ans = 0;
-    while (_dividend > _divisor) {
-        _dividend -= _divisor;
-        ans++;
+/// <summary>
+/// 快速除法
+/// 为避免 int 型溢出，参数都为负数。
+/// </summary>
+/// <param name="dividend"></param>
+/// <param name="divisor"></param>
+/// <returns></returns>
+int quickDiv(int dividend, int divisor) {
+    if (dividend > divisor) {
+        return 0;
     }
-    return ans * flag;
+
+    int ans = 1;
+    int sumDivisor = divisor;
+
+    while (sumDivisor > dividend - sumDivisor) {
+        // 翻倍
+        sumDivisor += sumDivisor;
+        ans += ans;
+    }
+
+    return ans + quickDiv(dividend - sumDivisor, divisor);
+}
+
+int Solution29::divide(int dividend, int divisor) {
+    if (dividend == 0) return 0;
+    if (divisor == 1) return dividend;
+    if (divisor == -1) {
+        if (dividend > INT_MIN) return -dividend;
+        return INT_MAX;
+    }
+
+    bool isNeg = (divisor < 0 && dividend < 0 || divisor>0 && dividend>0) ? 0 : 1;
+
+    dividend = dividend < 0 ? dividend : -dividend;
+    divisor = divisor < 0 ? divisor : -divisor;
+
+    return isNeg ? -quickDiv(dividend, divisor) : quickDiv(dividend, divisor);
 }

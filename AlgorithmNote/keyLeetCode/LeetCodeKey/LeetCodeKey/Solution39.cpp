51a7ae756d084381e71eb3747ac046e5c2a4bb55
Lviat Yi <LviatYi@qq.com>
2022-03-24 21:12:05 +0800

    Solve q39

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution39.cpp
@@ -0,0 +1,36 @@
+#include "Solution39.h"
+#include<algorithm>
+
+using namespace std;
+
+vector<int>currentAns;
+
+#pragma region 回溯
+/// <summary>
+/// 
+/// </summary>
+/// <param name="candidates"></param>
+/// <param name="ans"></param>
+/// <param name="start">起始可选的数字 index</param>
+/// <param name="target"></param>
+void dfs(vector<int>& candidates, vector<vector<int>>& ans, int start, int target) {
+    if (target < 0) return;
+    if (target == 0) {
+        ans.push_back(currentAns);
+        return;
+    }
+    for (int i = start; i < candidates.size(); i++) {
+        if (candidates[i] <= target) {
+            currentAns.push_back(candidates[i]);
+            dfs(candidates, ans, i, target - candidates[i]);
+            currentAns.pop_back();
+        }
+    }
+}
+
+vector<vector<int>> Solution39::combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>>ans;
+    dfs(candidates, ans, 0, target);
+    return ans;
+}
+#pragma endregion
---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution39.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+using std::vector;
+
+class Solution39 {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target);
+};
+

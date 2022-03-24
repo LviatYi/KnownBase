9fe49e4be7fcc7c4e46bfc790bb0ca08a8694dc0
Lviat Yi <LviatYi@qq.com>
2022-03-24 21:12:16 +0800

    Solve q40

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution40.cpp
@@ -0,0 +1,30 @@
+#include "Solution40.h"
+#include<algorithm>
+using namespace std;
+
+vector<int>currentAns;
+
+void dfs(vector<int>& candidates, vector<vector<int>>& ans, int start, int target) {
+    if (target < 0) return;
+    if (target == 0) {
+        ans.push_back(currentAns);
+        return;
+    }
+    for (int i = start; i < candidates.size(); i++) {
+        if (candidates[i] <= target) {
+            if (i != start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            currentAns.push_back(candidates[i]);
+            dfs(candidates, ans, i + 1, target - candidates[i]);
+            currentAns.pop_back();
+        }
+    }
+}
+
+vector<vector<int>> Solution40::combinationSum2(vector<int>& candidates, int target) {
+    sort(candidates.begin(), candidates.end());
+    vector<vector<int>>ans;
+    dfs(candidates, ans, 0, target);
+    return ans;
+}
---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution40.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<vector>
+using std::vector;
+class Solution40 {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target);
+};
+

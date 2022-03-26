b083a1ebf307dae1b9e667f9281241a1fc8b68de
Lviat Yi <LviatYi@qq.com>
2022-03-27 00:57:15 +0800

    Solve q41

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution41.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+using std::vector;
+class Solution41 {
+public:
+    int firstMissingPositive(vector<int>& nums);
+    int firstMissingPositive2(vector<int>& nums);
+};
+

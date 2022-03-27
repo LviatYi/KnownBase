90c54cec2251708b40241eec4e2b86ea5a5a8bd3
Lviat Yi <LviatYi@qq.com>
2022-03-27 21:24:39 +0800

    Solve s4

--- AlgorithmNote/keyLeetCode/LeetCodeKey/enc_temp_folder/eacadb1762b79667d91d5fbaded8/Sword4.cpp
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/enc_temp_folder/eacadb1762b79667d91d5fbaded8/Sword4.cpp
@@ -4,13 +4,13 @@ bool Sword4::findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
     if (!matrix.size()) {
         return false;
     }
-    if (!matrix[0].size()) {
-        return false;
-    }
     int i = 0;
     int j = matrix[0].size() - 1;
+    if (!j) {
+        return false;
+    }
 
-    while (i < matrix.size() && j >= 0) {
+    while (i < matrix.size() && j>=0) {
         if (matrix[i][j] > target) {
             j--;
         }

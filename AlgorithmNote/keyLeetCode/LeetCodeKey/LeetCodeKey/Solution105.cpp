7db847f3ebccae864b37941d655cd7e3baecb818
Lviat Yi <LviatYi@qq.com>
2022-03-27 21:24:18 +0800

    Update Solution105.cpp

--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution105.cpp
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution105.cpp
@@ -1,17 +1,5 @@
 #include "Solution105.h"
 
-TreeNode* Solution105::buildTree(vector<int>& preorder, vector<int>& inorder) {
-    // 前序遍历的首个为根结点。
-    TreeNode* ans = buildSubTree(
-        preorder.begin(),
-        preorder.end(),
-        inorder.begin(),
-        inorder.end()
-    );
-
-    return ans;
-}
-
 static TreeNode* buildSubTree(
     vector<int>::iterator preorderBegin, vector<int>::iterator preorderEnd, vector<int>::iterator inorderBegin, vector<int>::iterator inorderEnd) {
     TreeNode* ans = nullptr;
@@ -24,6 +12,18 @@ static TreeNode* buildSubTree(
         ans->left = buildSubTree(preorderBegin + 1, preorderBegin + lCount + 1, inorderBegin, rootPosInInorder);
         ans->right = buildSubTree(preorderBegin + lCount + 1, preorderEnd, rootPosInInorder + 1, inorderEnd);
     }
+
+    return ans;
+}
+
+TreeNode* Solution105::buildTree(vector<int>& preorder, vector<int>& inorder) {
+    // 前序遍历的首个为根结点。
+    TreeNode* ans = buildSubTree(
+        preorder.begin(),
+        preorder.end(),
+        inorder.begin(),
+        inorder.end()
+    );
 
     return ans;
 }

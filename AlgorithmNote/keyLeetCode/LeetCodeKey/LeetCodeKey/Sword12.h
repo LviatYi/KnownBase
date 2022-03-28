ebc7a3654b899a308a4bcee53b278deb41bdee75
Lviat Yi <LviatYi@qq.com>
2022-03-28 16:43:49 +0800

    Solve s12

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword12.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+class Sword12 {
+public:
+    bool exist(vector<vector<char>>& board, string word);
+    bool exist2(vector<vector<char>>& board, string word);
+};
+

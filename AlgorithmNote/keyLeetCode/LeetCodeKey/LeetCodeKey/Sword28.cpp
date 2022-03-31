2ac9e6c523d8a6aea6bd3360b04cc37800d78ec6
Lviat Yi <LviatYi@qq.com>
2022-04-01 01:21:47 +0800

    Solve s28

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword28.cpp
@@ -0,0 +1,26 @@
+#include "Sword28.h"
+
+bool isSame(TreeNode* r1, TreeNode* r2) {
+    if (!r1 && !r2) {
+        return true;
+    }
+    else if (r1 && r2) {
+        return r1->val == r2->val;
+    }
+    else {
+        return false;
+    }
+}
+
+bool isSubSymmetric(TreeNode* a, TreeNode* b) {
+    if (!a && !b) {
+        return true;
+    }
+
+    return isSame(a, b) && isSubSymmetric(a->left, b->right) && isSubSymmetric(a->right, b->left);
+}
+
+
+bool Sword28::isSymmetric(TreeNode* root) {
+        return isSubSymmetric(root,root);
+}
---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword28.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "TreeNode.h"
+class Sword28 {
+public:
+    bool isSymmetric(TreeNode* root);
+};
+

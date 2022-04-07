a53c808b8c6143660919615860881bf58d3bda90
Lviat Yi <LviatYi@qq.com>
2022-04-08 00:29:36 +0800

    Solve q946

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution946.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<vector>
+
+using std::vector;
+class Solution946 {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped);
+    bool validateStackSequences2(vector<int>& pushed, vector<int>& popped);
+};
+

11dd2fa377499ddcf7c6b96038f9a32ff10f32bc
Lviat Yi <LviatYi@qq.com>
2022-04-09 15:37:09 +0800

    Update ListNode.h

--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/ListNode.h
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/ListNode.h
@@ -3,7 +3,9 @@
 struct ListNode {
     int val;
     ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ListNode* pre;
+    ListNode() : val(0), next(nullptr), pre(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr), pre(nullptr) {}
+    ListNode(int x, ListNode* _next) : val(x), next(_next), pre(nullptr) {}
+    ListNode(int x, ListNode* _next, ListNode* _pre) : val(x), next(_next), pre(_pre) {}
 };

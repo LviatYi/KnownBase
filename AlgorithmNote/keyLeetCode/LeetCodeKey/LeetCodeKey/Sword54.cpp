b35be0aab81b35c130644e50922fd139e7ffb895
Lviat Yi <LviatYi@qq.com>
2022-04-14 21:43:27 +0800

    Solve s54

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword54.cpp
@@ -0,0 +1,21 @@
+#include "Sword54.h"
+
+int invInOrder(TreeNode* root, int& k) {
+    if (!root) {
+        return -1;
+    }
+    int right = invInOrder(root->right, k);
+    if (right != -1) {
+        return right;
+    }
+    if (k == 1) {
+        return root->val;
+    }
+        k--;
+
+    return invInOrder(root->left, k);
+}
+
+int Sword54::kthLargest(TreeNode* root, int k) {
+    return invInOrder(root, k);
+}

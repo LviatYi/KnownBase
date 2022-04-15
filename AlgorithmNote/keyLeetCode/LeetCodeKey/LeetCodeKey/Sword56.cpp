5e88afa1a66ffd8dce58b32f38f33e4bcebe814b
Lviat Yi <LviatYi@qq.com>
2022-04-15 16:49:12 +0800

    Solve s56

--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword56.cpp
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword56.cpp
@@ -1,7 +1,10 @@
 #include "Sword56.h"
 #include<set>
+#include<algorithm>
 using std::set;
+using std::max;
 
+#pragma region Sword56_1
 vector<int> Sword56::singleNumbers(vector<int>& nums) {
     // 设两不同数为 x,y
     int ret = 0;
@@ -31,3 +34,65 @@ vector<int> Sword56::singleNumbers(vector<int>& nums) {
 
     return { a,b };
 }
+#pragma endregion
+#pragma region Sword56_2
+
+#pragma region 定义三进制异或运算
+class ternary {
+public:
+    long long val;
+    ternary(long _val) :val(_val) {}
+    const ternary operator^(const ternary& rhs) {
+        long long lval = this->val;
+        long long rval = rhs.val;
+        long long i = 1;
+        long long greater = max(lval, rval);
+        long long ans = 0;
+
+        while (greater - 3 * i > 0) {
+            i *= 3;
+        }
+
+        while (i > 0 && (lval != 0 || rval != 0)) {
+            long long a = lval / i;
+            long long b = rval / i;
+
+            ans += (a + b) % 3 * i;
+
+            lval -= a * i;
+            rval -= b * i;
+
+            i /= 3;
+        }
+        return ternary(ans);
+    }
+
+    ternary& operator^=(const ternary& rhs) {
+        *this = (*this) ^ rhs;
+        return *this;
+    }
+};
+
+int Sword56::singleNumber(vector<int>& nums) {
+    ternary ret(0);
+
+    for (int num : nums) {
+        ret ^= ternary(num);
+    }
+
+    return ret.val;
+}
+#pragma endregion
+
+#pragma region 有限状态自动机
+int Sword56::singleNumber2(vector<int>& nums) {
+    int ones = 0, twos = 0;
+    for (int num : nums) {
+        ones = ones ^ num & ~twos;
+        twos = twos ^ num & ~ones;
+    }
+    return ones;
+}
+#pragma endregion
+
+#pragma endregion
--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword56.h
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword56.h
@@ -5,5 +5,7 @@ using std::vector;
 class Sword56 {
 public:
     vector<int> singleNumbers(vector<int>& nums);
+    int singleNumber(vector<int>& nums);
+    int singleNumber2(vector<int>& nums);
 };
 

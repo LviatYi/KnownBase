a11fcc4ecc90b2fbf005efe455dd8d4e805fffa1
Lviat Yi <LviatYi@qq.com>
2022-04-15 18:25:53 +0800

    Solve s58

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword58.cpp
@@ -0,0 +1,23 @@
+#include "Sword58.h"
+
+string Sword58::reverseWords(string s) {
+    string ans;
+    string::iterator begin = s.end() - 1;
+    string::iterator end = s.end();
+
+    while (begin != s.begin() - 1) {
+        if (*begin == ' ') {
+            if (begin + 1 != end) {
+                ans += string(begin + 1, end) + " ";
+            }
+            end = begin;
+        }
+        begin--;
+    }
+    ans += string(begin + 1, end);
+    if (ans.at(ans.size() - 1) == ' ') {
+        ans.erase(ans.size() - 1, 1);
+    }
+
+    return ans;
+}
---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword58.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<string>
+
+using std::string;
+
+class Sword58 {
+public:
+    string reverseWords(string s);
+};
+

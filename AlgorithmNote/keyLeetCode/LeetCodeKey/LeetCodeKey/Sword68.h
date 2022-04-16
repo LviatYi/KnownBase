067dd0723fe01387cc313852ded1897a6edb06e9
Lviat Yi <LviatYi@qq.com>
2022-04-17 01:13:48 +0800

    Solve s68

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword68.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include "TreeNode.h"
+
+class Sword68 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q);
+    TreeNode* lowestCommonAncestor2(TreeNode* root, TreeNode* p, TreeNode* q);    
+};

67dbfe4cbfce35245359bd45231f258235d4e6ee
Lviat Yi <LviatYi@qq.com>
2022-04-16 16:19:13 +0800

    Solve s59

--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword59.cpp
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword59.cpp
@@ -1,11 +1,10 @@
 #include "Sword59.h"
 #include<algorithm>
-#include<queue>
 
 using std::priority_queue;
-using std::greater;
 using std::pair;
 
+#pragma region Sword59_1
 #pragma region 优先队列
 struct less {
     bool operator()(pair<int, int> lhs, pair<int, int> rhs) {
@@ -38,9 +37,6 @@ vector<int> Sword59::maxSlidingWindow(vector<int>& nums, int k) {
 }
 #pragma endregion
 #pragma region 单调队列
-#include<deque>
-using std::deque;
-
 vector<int> Sword59::maxSlidingWindow2(vector<int>& nums, int k) {
     int n = nums.size();
     deque<int> q;
@@ -65,3 +61,36 @@ vector<int> Sword59::maxSlidingWindow2(vector<int>& nums, int k) {
     return ans;
 }
 #pragma endregion
+#pragma endregion
+
+#pragma region Sword59_2
+Sword59::Sword59() {
+}
+int Sword59::max_value() {
+    if (this->maxValue.empty()) {
+        return -1;
+    }
+    return this->maxValue.front();
+}
+void Sword59::push_back(int value) {
+        this->q.push(value);
+        while (!this->maxValue.empty() && this->maxValue.back() < value) {
+            this->maxValue.pop_back();
+        }
+        this->maxValue.push_back(value);
+}
+int Sword59::pop_front() {
+    if (this->q.empty()) {
+        return -1;
+    }
+    int ans = this->q.front();
+    if (maxValue.front() == ans) {
+        maxValue.pop_front();
+    }
+
+    this->q.pop();
+    return ans;
+}
+#pragma endregion
+
+
--- AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword59.h
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword59.h
@@ -1,10 +1,22 @@
 #pragma once
 #include<vector>
+#include<queue>
+#include<deque>
 using std::vector;
+using std::queue;
+using std::deque;
 
 class Sword59 {
+private:
+    deque<int> maxValue;
+    queue<int> q;
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k);
     vector<int> maxSlidingWindow2(vector<int>& nums, int k);
+
+    Sword59();
+    int max_value();
+    void push_back(int value);
+    int pop_front();
 };
 

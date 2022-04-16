9a362f16773b355e39dfb7329512aaec1aba3a0b
Lviat Yi <LviatYi@qq.com>
2022-04-17 01:17:13 +0800

    Solve q235

---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution235.cpp
@@ -0,0 +1,19 @@
+﻿#include "Solution235.h"
+
+TreeNode* Solution235::lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* ans = root;
+
+    while (true) {
+        if (p->val < ans->val && q->val < ans->val) {
+            ans = ans->left;
+        }
+        else if (p->val > ans->val && q->val > ans->val) {
+            ans = ans->right;
+        }
+        else {
+            break;
+        }
+    }
+
+    return ans;
+}
---
+++ AlgorithmNote/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution235.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include "TreeNode.h"
+
+class Solution235 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q);
+    
+};

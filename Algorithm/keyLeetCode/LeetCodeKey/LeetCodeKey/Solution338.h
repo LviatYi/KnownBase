0c8ed039762a9229b4722f6f473f4ba7d4e19526
Lviat Yi <LviatYi@foxmail.com>
2022-08-02 16:13:17 +0800

    Solve solution338

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution338.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <vector>
+
+using namespace std;
+
+class Solution338 {
+public:
+	vector<int> countBits(int n);
+	vector<int> countBits2(int n);
+	vector<int> countBits3(int n);
+};

7bb0d527c74d0c188d8488d850d768fe4f962aae
Lviat Yi <LviatYi@foxmail.com>
2022-08-05 01:14:41 +0800

    Solve Solution167

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution167.cpp
@@ -0,0 +1,17 @@
+﻿#include "Solution167.h"
+
+vector<int> Solution167::twoSum(vector<int>& numbers, int target) {
+    int left = 0, right = numbers.size() - 1;
+    int curr_sum = numbers.at(left) + numbers.at(right);
+    while (curr_sum != target) {
+        if (curr_sum < target) {
+            left++;
+        }
+        else {
+            right--;
+        }
+        curr_sum = numbers.at(left) + numbers.at(right);
+    }
+
+    return {left + 1, right + 1};
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution167.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <vector>
+
+using namespace std;
+
+class Solution167 {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target);
+};

7084442274a56d1c504097df2d50260187ba6e32
Lviat Yi <LviatYi@foxmail.com>
2023-01-31 16:51:14 +0800

    feat: add solution of solution 70

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution70.cpp
@@ -0,0 +1,11 @@
+﻿#include "Solution70.h"
+
+int Solution70::climbStairs(int n) {
+    if (n > 2) {
+        if (!dp2[n]) {
+            dp2[n] = climbStairs(n - 1) + climbStairs(n - 2);
+            dp2[n] %= 1000000007;
+        }
+    }
+    return dp2[n];
+}

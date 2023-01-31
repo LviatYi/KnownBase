f376330e1f8e3a4f30421f52b8f73a76a6774cb1
Lviat Yi <LviatYi@foxmail.com>
2023-01-31 16:49:57 +0800

    feat: update solution of sword3

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword3.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword3.cpp
@@ -13,3 +13,18 @@ int Sword3::findRepeatNumber(vector<int>& nums) {
 
     return 0;
 }
+
+int Sword3::findRepeatNumber2(vector<int>& nums) {
+    int t;
+    for (int i = 0; i < nums.size(); ++i) {
+        while (nums[i] != i) {
+            if (nums[i] == nums[nums[i]]) {
+                return nums[i];
+            }
+            t = nums[i];
+            nums[i] = nums[t];
+            nums[t] = t;
+        }
+    }
+    return 0;
+}
--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword3.h
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Sword3.h
@@ -4,6 +4,7 @@
 using std::vector;
 
 class Sword3 {
+public:
     int findRepeatNumber(vector<int>& nums);
+    int findRepeatNumber2(vector<int>& nums);
 };
-

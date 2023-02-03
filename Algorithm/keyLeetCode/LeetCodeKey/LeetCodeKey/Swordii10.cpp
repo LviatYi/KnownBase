9f13a8e940e17f585df0cda4422e880145d71751
Lviat Yi <LviatYi@foxmail.com>
2023-02-03 14:31:46 +0800

    feat: add keys swordii10

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii10.cpp
@@ -0,0 +1,17 @@
+﻿#include "Swordii10.h"
+
+#include <unordered_map>
+
+int Swordii10::subarraySum(std::vector<int>& nums, int k) {
+    std::unordered_map<int, int> mp;
+    mp[0] = 1;
+    int count = 0, pre = 0;
+    for (auto& x : nums) {
+        pre += x;
+        if (mp.find(pre - k) != mp.end()) {
+            count += mp[pre - k];
+        }
+        mp[pre]++;
+    }
+    return count;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii10.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <vector>
+
+class Swordii10 {
+public:
+    int subarraySum(std::vector<int>& nums, int k);
+
+};

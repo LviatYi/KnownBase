7d4e195414dd769657bb09cf754186496cd6e1e6
Lviat Yi <LviatYi@foxmail.com>
2023-02-06 16:28:08 +0800

    feat: add key of Swordii14

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii14.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <string>
+
+class Swordii14 {
+public:
+    bool checkInclusion(std::string s1, std::string s2);
+    bool checkInclusion2(std::string s1, std::string s2);
+};

4d432504c9c322e6febee81c594d2066a5e2ea8f
Lviat Yi <LviatYi@foxmail.com>
2023-02-07 14:17:33 +0800

    feat: add key of Swordii20

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii20.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <string>
+
+class Swordii20 {
+
+private:
+    int countSubstrings(const std::string& s, int begin, int end);
+public:
+    int countSubstrings(std::string s);
+};

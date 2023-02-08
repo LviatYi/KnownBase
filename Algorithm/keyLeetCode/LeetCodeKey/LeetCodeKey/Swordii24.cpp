a3de604525a083abc8cd08f2aabb9ae8a22bd103
Lviat Yi <LviatYi@foxmail.com>
2023-02-08 13:03:27 +0800

    feat: add key of Swordii24

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii24.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii24.cpp
@@ -5,18 +5,15 @@ ListNode* Swordii24::reverseList(ListNode* head) {
     if (!head || !head->next) {
         return head;
     }
-    ListNode* pre_head = new ListNode(0, head);
-    ListNode* former = pre_head;
-    ListNode* middle = head;
-    ListNode* latter = head->next;
-    ListNode* p = latter->next;
 
-    do {
-        former->next = latter;
-        middle->next = latter->next;
-        latter->next = middle;
+    ListNode* p_head = head;
+
+    while (head->next) {
+        ListNode* temp = head->next->next;
+        head->next->next = p_head;
+        p_head = head->next;
+        head->next = temp;
     }
-    while (latter->next);
 
-    return nullptr;
+    return p_head;
 }

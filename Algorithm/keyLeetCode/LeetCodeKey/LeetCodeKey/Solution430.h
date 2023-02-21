dcc3fa855da00e62ce04ffe12ced149b1d4be22c
Lviat Yi <LviatYi@foxmail.com>
2023-02-21 15:09:32 +0800

    feat: add key of Solution430

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution430.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+
+    Node(int _val);
+};
+
+
+class Solution430 {
+private:
+    Node* subFlatten(Node* head);
+public:
+    Node* flatten(Node* head);
+    Node* flatten2(Node* head);
+};

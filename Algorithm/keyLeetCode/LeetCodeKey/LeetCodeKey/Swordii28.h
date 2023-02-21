e091799d9457019153c6a6516159f9d9ea9fb217
Lviat Yi <LviatYi@foxmail.com>
2023-02-21 15:10:00 +0800

    feat: add key of Swordii28

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii28.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+
+    Node(int _val);
+};
+
+class Swordii28 {
+private:
+    Node* subFlatten(Node* head);
+public:
+    Node* flatten(Node* head);
+    Node* flatten2(Node* head);
+};

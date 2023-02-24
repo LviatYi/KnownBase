35766d094ab09293953a156d640898a500125145
Lviat Yi <LviatYi@foxmail.com>
2023-02-24 15:45:21 +0800

    feat: add key of Solution739

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution739.cpp
@@ -0,0 +1,26 @@
+﻿#include "Solution739.h"
+
+std::vector<int> Solution739::dailyTemperatures(std::vector<int>& temperatures) {
+    if (temperatures.empty()) {
+        return {};
+    }
+
+    std::stack<std::pair<int, int>> stack = {};
+    std::vector<int> ret = std::vector<int>(temperatures.size(), 0);
+    for (int i = temperatures.size() - 1; i >= 0; --i) {
+        while (!stack.empty() && stack.top().first <= temperatures[i]) {
+            stack.pop();
+        }
+
+        if (stack.empty()) {
+            stack.push({temperatures[i], i});
+        }
+        else {
+            ret[i] = stack.top().second - i;
+            stack.push({temperatures[i], i});
+        }
+
+    }
+
+    return ret;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution739.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <stack>
+#include <vector>
+
+class Solution739 {
+public:
+    std::vector<int> dailyTemperatures(std::vector<int>& temperatures);
+};

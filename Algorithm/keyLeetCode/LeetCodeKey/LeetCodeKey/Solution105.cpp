32612771382f23a84b1a783486648fc4a401759a
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:47:21 +0800

    feat: add key of Solution105

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution105.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution105.cpp
@@ -1,11 +1,11 @@
 #include "Solution105.h"
 
-static Swordii43TreeNode* buildSubTree(
+static TreeNode* buildSubTree(
     vector<int>::iterator preorderBegin, vector<int>::iterator preorderEnd, vector<int>::iterator inorderBegin,
     vector<int>::iterator inorderEnd) {
-    Swordii43TreeNode* ans = nullptr;
+    TreeNode* ans = nullptr;
     if (preorderBegin < preorderEnd) {
-        ans = new Swordii43TreeNode(*preorderBegin);
+        ans = new TreeNode(*preorderBegin);
         vector<int>::iterator rootPosInInorder = find(inorderBegin, inorderEnd, *preorderBegin);
 
         int lCount = rootPosInInorder - inorderBegin;
@@ -17,9 +17,9 @@ static Swordii43TreeNode* buildSubTree(
     return ans;
 }
 
-Swordii43TreeNode* Solution105::buildTree(vector<int>& preorder, vector<int>& inorder) {
+TreeNode* Solution105::buildTree(vector<int>& preorder, vector<int>& inorder) {
     // 前序遍历的首个为根结点。
-    Swordii43TreeNode* ans = buildSubTree(
+    TreeNode* ans = buildSubTree(
         preorder.begin(),
         preorder.end(),
         inorder.begin(),
--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution105.h
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution105.h
@@ -1,9 +1,9 @@
 #pragma once
-#include"Swordii43TreeNode.h"
+#include"TreeNode.h"
 #include<vector>
 using std::vector;
 
 class Solution105 {
 public:
-    Swordii43TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder);
 };

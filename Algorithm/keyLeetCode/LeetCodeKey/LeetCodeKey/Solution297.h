678cafbd7c6ab909ac3b478824d4a1e52c7223c1
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:48:29 +0800

    feat: update key of Solution297

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution297.h
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution297.h
@@ -1,14 +1,14 @@
 #pragma once
 #include<string>
-#include"Swordii43TreeNode.h"
+#include"TreeNode.h"
 
 using std::string;
 
 class Solution297 {
 public:
     // Encodes a tree to a single string.
-    string serialize(Swordii43TreeNode* root);
+    string serialize(TreeNode* root);
 
     // Decodes your encoded data to tree.
-    Swordii43TreeNode* deserialize(string data);
+    TreeNode* deserialize(string data);
 };

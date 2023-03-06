81a80d9ff8c1aa942ae1f651880bb3488b0a71c2
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:50:44 +0800

    feat: add key of Swordii43

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii43.cpp
@@ -0,0 +1,52 @@
+﻿#include "Swordii43.h"
+
+#include <immintrin.h>
+#include <queue>
+
+Swordii43::Swordii43(Swordii43TreeNode* root) {
+    this->root_ = root;
+
+    std::queue<Swordii43TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        ++cnt_;
+        Swordii43TreeNode* node = q.front();
+        q.pop();
+        if (node->left) {
+            q.push(node->left);
+        }
+        if (node->right) {
+            q.push(node->right);
+        }
+    }
+}
+
+int Swordii43::insert(int v) {
+    ++cnt_;
+    Swordii43TreeNode* child = new Swordii43TreeNode(v);
+    Swordii43TreeNode* node = root_;
+    // in gcc
+    //int highbit = 31 - __builtin_clz(cnt);
+    // in msvc
+    int highbit = 31 - _lzcnt_u64(cnt_);
+    for (int i = highbit - 1; i >= 1; --i) {
+        if (cnt_ & (1 << i)) {
+            node = node->right;
+        }
+        else {
+            node = node->left;
+        }
+    }
+    if (cnt_ & 1) {
+        node->right = child;
+    }
+    else {
+        node->left = child;
+    }
+    return node->val;
+}
+
+Swordii43TreeNode* Swordii43::get_root() {
+    return root_;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii43.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+struct Swordii43TreeNode {
+    int val;
+    Swordii43TreeNode* left;
+    Swordii43TreeNode* right;
+
+    Swordii43TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    Swordii43TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    Swordii43TreeNode(int x, Swordii43TreeNode* left, Swordii43TreeNode* right) : val(x), left(left), right(right) {
+    }
+};
+
+class Swordii43 {
+private:
+    int cnt_ = 0;
+    Swordii43TreeNode* root_;
+public:
+    Swordii43(Swordii43TreeNode* root);
+    int insert(int v);
+    Swordii43TreeNode* get_root();
+};

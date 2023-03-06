b71aac544821d54d6084e7eca80703f7afad290f
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:50:34 +0800

    feat: add key of Swordii41

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii41.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class Swordii41 {
+private:
+    int* numbers_;
+    int capacity_;
+    int length_;
+    int tail_;
+    int sum_;
+public:
+    /** Initialize your data structure here. */
+    Swordii41(int size);
+
+    double next(int val);
+};

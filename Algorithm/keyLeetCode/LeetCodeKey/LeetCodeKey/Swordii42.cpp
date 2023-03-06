52054b9d1866b6eef90b6ced084dc3e385e444de
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:50:39 +0800

    feat: add key of Swordii42

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii42.cpp
@@ -0,0 +1,14 @@
+﻿#include "Swordii42.h"
+
+Swordii42::Swordii42() {
+    queue_ = {};
+}
+
+int Swordii42::ping(int t) {
+    queue_.push(t);
+    while (queue_.front() < t - retention_) {
+        queue_.pop();
+    }
+
+    return queue_.size();
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii42.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <queue>
+
+class Swordii42 {
+private:
+    const int retention_ = 3000;
+    std::queue<int> queue_;
+public:
+    Swordii42();
+    int ping(int t);
+};

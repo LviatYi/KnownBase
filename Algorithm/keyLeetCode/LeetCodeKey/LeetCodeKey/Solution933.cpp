586c5b01f3ca52847692a6c35ba1643f36229f0a
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:50:08 +0800

    feat: add key of Solution933

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution933.cpp
@@ -0,0 +1,14 @@
+﻿#include "Solution933.h"
+
+Solution933::Solution933() {
+    queue_ = {};
+}
+
+int Solution933::ping(int t) {
+    queue_.push(t);
+    while (queue_.front() < t - retention_) {
+        queue_.pop();
+    }
+
+    return queue_.size();
+}

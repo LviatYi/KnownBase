f1dcf392ad98224da0f8597a1c706be2ed4e035a
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:50:19 +0800

    feat: add key of Swordii39

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii39.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <vector>
+
+class Swordii39 {
+public:
+    int largestRectangleArea(std::vector<int>& heights);
+    int largestRectangleArea2(std::vector<int>& heights);
+};

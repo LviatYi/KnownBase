b2eec2794d27846769906414fcfdde25ba860b34
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:49:48 +0800

    feat: add key of Solution919

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution919.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+
+struct Solution919TreeNode {
+    int val;
+    Solution919TreeNode* left;
+    Solution919TreeNode* right;
+
+    Solution919TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    Solution919TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    Solution919TreeNode(int x, Solution919TreeNode* left,
+                        Solution919TreeNode* right) : val(x), left(left), right(right) {
+    }
+};
+
+class Solution919 {
+private:
+    int cnt_ = 0;
+    Solution919TreeNode* root_;
+public:
+    Solution919(Solution919TreeNode* root);
+    int insert(int v);
+    Solution919TreeNode* get_root();
+};

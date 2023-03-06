3c6cdc8fde0d43389cd8077ce16ae7fb0ab9d32a
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:49:32 +0800

    feat: add key of Solution346

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution346.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class Solution346 {
+private:
+    int* numbers_;
+    int capacity_;
+    int length_;
+    int tail_;
+    int sum_;
+public:
+    /** Initialize your data structure here. */
+    Solution346(int size);
+
+    double next(int val);
+};

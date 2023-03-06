e271dbd218b52f9193fa53f785cd38642079ab61
Lviat Yi <LviatYi@foxmail.com>
2023-03-06 13:43:45 +0800

    feat: add key of Solution235

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution235.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution235.cpp
@@ -1,21 +1,22 @@
 ﻿#include "Solution235.h"
 
 #pragma region 模拟
-TreeNode* Solution235::lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-	TreeNode* ans = root;
+Swordii43TreeNode* Solution235::lowestCommonAncestor(Swordii43TreeNode* root, Swordii43TreeNode* p,
+                                                     Swordii43TreeNode* q) {
+    Swordii43TreeNode* ans = root;
 
-	while (true) {
-		if (p->val < ans->val && q->val < ans->val) {
-			ans = ans->left;
-		}
-		else if (p->val > ans->val && q->val > ans->val) {
-			ans = ans->right;
-		}
-		else {
-			break;
-		}
-	}
+    while (true) {
+        if (p->val < ans->val && q->val < ans->val) {
+            ans = ans->left;
+        }
+        else if (p->val > ans->val && q->val > ans->val) {
+            ans = ans->right;
+        }
+        else {
+            break;
+        }
+    }
 
-	return ans;
+    return ans;
 }
 #pragma endregion

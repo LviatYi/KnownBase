774753795f954d7696f639896256cee8c4bb5c98
Lviat Yi <LviatYi@foxmail.com>
2023-03-09 14:14:38 +0800

    feat: update key of Swordii48

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii48.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii48.cpp
@@ -1,18 +1,44 @@
 ﻿#include "Swordii48.h"
 
-void Swordii48::subSerialize(TreeNode* root, std::string& ans) {
+#include "LeetcodeUtil.h"
+
+void Swordii48::subSerialize(TreeNode* root, std::vector<std::string>& ans) {
     if (!root) {
-        ans.append("null,");
+        ans.push_back("null");
         return;
     }
-    ans.append(std::to_string(root->val) + ",");
+    ans.push_back(std::to_string(root->val));
 
     subSerialize(root->left, ans);
     subSerialize(root->right, ans);
 }
 
+std::string Swordii48::serialize(TreeNode* root, bool zip) {
+    std::vector<std::string> words;
+    subSerialize(root, words);
+
+    if (words.empty()) {
+        return "null";
+    }
+
+    if (zip) {
+        int r = words.size() - 1;
+        while (words[r--] == "null") {
+            words.pop_back();
+        }
+    }
+
+    std::string ans = words[0];
+
+    for (int i = 1; i < words.size(); ++i) {
+        ans.append(",").append(words[i]);
+    }
+
+    return ans;
+}
+
 void Swordii48::subDeserialize(std::vector<std::string>& datas, int& i, TreeNode* root) {
-    if (datas.at(i) == "null") {
+    if (i >= datas.size() || datas.at(i) == "null") {
         i++;
         root->left = nullptr;
     }
@@ -21,7 +47,7 @@ void Swordii48::subDeserialize(std::vector<std::string>& datas, int& i, TreeNode
         i++;
         subDeserialize(datas, i, root->left);
     }
-    if (datas.at(i) == "null") {
+    if (i >= datas.size() || datas.at(i) == "null") {
         i++;
         root->right = nullptr;
     }
@@ -30,28 +56,13 @@ void Swordii48::subDeserialize(std::vector<std::string>& datas, int& i, TreeNode
         i++;
         subDeserialize(datas, i, root->right);
     }
-    return;
-}
-
-std::string Swordii48::serialize(TreeNode* root) {
-    std::string ans;
-    subSerialize(root, ans);
-
-    return ans;
 }
 
 TreeNode* Swordii48::deserialize(std::string data) {
     std::vector<std::string> datas;
     std::string word;
-    for (char c : data) {
-        if (c != ',') {
-            word += c;
-        }
-        else {
-            datas.push_back(word);
-            word.clear();
-        }
-    }
+
+    datas = LeetcodeUtil::string_split(data, ',');
 
     if (datas.empty() || datas.at(0) == "null") {
         return nullptr;
--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii48.h
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii48.h
@@ -6,11 +6,11 @@
 
 class Swordii48 {
 public:
-    void subSerialize(TreeNode* root, std::string& ans);
+    void subSerialize(TreeNode* root, std::vector<std::string>& ans);
 
     void subDeserialize(std::vector<std::string>& datas, int& i, TreeNode* root);
 
-    std::string serialize(TreeNode* root);
+    std::string serialize(TreeNode* root, bool zip = false);
 
     TreeNode* deserialize(std::string data);
     // Your Codec object will be instantiated and called as such:

aac4b8df6abdd95d489d0cc1fa32a619b68d7ad4
Lviat Yi <LviatYi@foxmail.com>
2023-03-09 10:16:01 +0800

    feat: add key of Solution814

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution814.cpp
@@ -0,0 +1,27 @@
+﻿#include "Solution814.h"
+
+bool Solution814::needCutSub(TreeNode* sub) {
+    if (!sub) {
+        return false;
+    }
+
+    if (sub->left && needCutSub(sub->left)) {
+        // delete sub->left;
+        sub->left = nullptr;
+    }
+
+    if (sub->right && needCutSub(sub->right)) {
+        // delete sub->right;
+        sub->right = nullptr;
+    }
+    return !sub->left && !sub->right && sub->val == 0;
+}
+
+TreeNode* Solution814::pruneTree(TreeNode* root) {
+    if (needCutSub(root)) {
+        // delete root;
+        return nullptr;
+    }
+
+    return root;
+}

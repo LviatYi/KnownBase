55d8895cfa88523f44ff3422fb4d351eed1da136
Lviat Yi <LviatYi@foxmail.com>
2023-03-09 10:14:05 +0800

    feat: add key of Solution199

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution199.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <vector>
+
+#include "TreeNode.h"
+
+class Solution199 {
+private:
+    void dfs(TreeNode* root, std::vector<int>& ret, int depth);
+public:
+    std::vector<int> rightSideView(TreeNode* root);
+    std::vector<int> rightSideView2(TreeNode* root);
+};

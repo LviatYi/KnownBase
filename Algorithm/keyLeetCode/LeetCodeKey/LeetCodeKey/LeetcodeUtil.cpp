4c60e7dfb3795b6dd22bfda1b14fc483426c0f17
Lviat Yi <LviatYi@foxmail.com>
2023-03-09 10:01:48 +0800

    feat: update LeetcodeUtil
    add TreeNode serialize

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.cpp
@@ -1,6 +1,6 @@
 ﻿#include "LeetcodeUtil.h"
 
-std::vector<std::vector<int>> LeetcodeUtil::deserialization_vector_vector(std::string in) {
+std::vector<std::vector<int>> LeetcodeUtil::deserialize_vector_vector(std::string in) {
     std::vector<std::vector<int>> ret = {};
 
     if (in.empty()) {
@@ -50,4 +50,66 @@ std::vector<std::vector<int>> LeetcodeUtil::deserialization_vector_vector(std::s
         return ret;
     }
     return {};
+}
+
+static void subDeserialize(std::vector<std::string>& datas, int& i, TreeNode* root) {
+    if (datas.at(i) == "null") {
+        i++;
+        root->left = nullptr;
+    }
+    else {
+        root->left = new TreeNode(std::stoi(datas.at(i)));
+        i++;
+        subDeserialize(datas, i, root->left);
+    }
+    if (datas.at(i) == "null") {
+        i++;
+        root->right = nullptr;
+    }
+    else {
+        root->right = new TreeNode(std::stoi(datas.at(i)));
+        i++;
+        subDeserialize(datas, i, root->right);
+    }
+    return;
+}
+
+TreeNode* LeetcodeUtil::deserialize_tree_node(std::string data) {
+    std::vector<std::string> datas;
+    std::string word;
+    for (char c : data) {
+        if (c != ',') {
+            word += c;
+        }
+        else {
+            datas.push_back(word);
+            word.clear();
+        }
+    }
+
+    if (datas.empty() || datas.at(0) == "null") {
+        return nullptr;
+    }
+    int i = 0;
+    TreeNode* root = new TreeNode(std::stoi(datas.at(i++)));
+    subDeserialize(datas, i, root);
+    return root;
+}
+
+static void subSerialize(TreeNode* root, std::string& ans) {
+    if (!root) {
+        ans.append("null,");
+        return;
+    }
+    ans.append(std::to_string(root->val) + ",");
+
+    subSerialize(root->left, ans);
+    subSerialize(root->right, ans);
+}
+
+std::string LeetcodeUtil::serialize_tree_node(TreeNode* root) {
+    std::string ans;
+    subSerialize(root, ans);
+
+    return ans;
 }
--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.h
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.h
@@ -2,7 +2,11 @@
 #include <string>
 #include <vector>
 
+#include "TreeNode.h"
+
 class LeetcodeUtil {
 public:
-    static std::vector<std::vector<int>> deserialization_vector_vector(std::string in);
+    static std::vector<std::vector<int>> deserialize_vector_vector(std::string in);
+    static TreeNode* deserialize_tree_node(std::string data);
+    static std::string serialize_tree_node(TreeNode* root);
 };

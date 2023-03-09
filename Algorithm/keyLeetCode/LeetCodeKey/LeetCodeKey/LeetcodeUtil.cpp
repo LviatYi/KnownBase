f23cbbb7c99721fa562de6a25fb97c25d7dabb46
Lviat Yi <LviatYi@foxmail.com>
2023-03-09 14:14:22 +0800

    feat: update LeetcodeUtil
    update treenode serialization

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.cpp
@@ -1,5 +1,12 @@
 ﻿#include "LeetcodeUtil.h"
 
+#include <list>
+#include <queue>
+
+#pragma region Serialize
+
+#pragma region vector vector int
+
 std::vector<std::vector<int>> LeetcodeUtil::deserialize_vector_vector(std::string in) {
     std::vector<std::vector<int>> ret = {};
 
@@ -14,27 +21,26 @@ std::vector<std::vector<int>> LeetcodeUtil::deserialize_vector_vector(std::strin
     bool element_valid = false;
     bool vector_valid = false;
     int number = 0;
-    std::vector<int> current_vector = std::vector<int>();
+    auto current_vector = std::vector<int>();
 
-
-    for (int i = 0; i < in.size(); ++i) {
-        if (in[i] == '[') {
+    for (const char i : in) {
+        if (i == '[') {
             element_valid = true;
             vector_valid = true;
             --squareBracketCount;
         }
-        else if (std::isdigit(in[i])) {
+        else if (std::isdigit(i)) {
             number *= 10;
-            number += in[i] - '0';
+            number += i - '0';
         }
 
-        else if (in[i] == ',' || in[i] == ']') {
+        else if (i == ',' || i == ']') {
             if (element_valid) {
                 current_vector.push_back(number);
                 number = 0;
             }
 
-            if (in[i] == ']') {
+            if (i == ']') {
                 --squareBracketCount;
                 if (vector_valid) {
                     ret.push_back(current_vector);
@@ -52,64 +58,189 @@ std::vector<std::vector<int>> LeetcodeUtil::deserialize_vector_vector(std::strin
     return {};
 }
 
-static void subDeserialize(std::vector<std::string>& datas, int& i, TreeNode* root) {
-    if (datas.at(i) == "null") {
+#pragma endregion
+
+#pragma region treenode
+
+static void sub_preorder_deserialize_tree_node(std::vector<std::string>& datas, int& i, TreeNode* root) {
+    if (i >= datas.size() || datas.at(i) == "null") {
         i++;
         root->left = nullptr;
     }
     else {
         root->left = new TreeNode(std::stoi(datas.at(i)));
         i++;
-        subDeserialize(datas, i, root->left);
+        sub_preorder_deserialize_tree_node(datas, i, root->left);
     }
-    if (datas.at(i) == "null") {
+    if (i >= datas.size() || datas.at(i) == "null") {
         i++;
         root->right = nullptr;
     }
     else {
         root->right = new TreeNode(std::stoi(datas.at(i)));
         i++;
-        subDeserialize(datas, i, root->right);
+        sub_preorder_deserialize_tree_node(datas, i, root->right);
     }
-    return;
 }
 
-TreeNode* LeetcodeUtil::deserialize_tree_node(std::string data) {
+TreeNode* LeetcodeUtil::preorder_deserialize_tree_node(std::string data) {
     std::vector<std::string> datas;
     std::string word;
-    for (char c : data) {
-        if (c != ',') {
-            word += c;
-        }
-        else {
-            datas.push_back(word);
-            word.clear();
-        }
-    }
+
+    datas = LeetcodeUtil::string_split(data, ',');
 
     if (datas.empty() || datas.at(0) == "null") {
         return nullptr;
     }
     int i = 0;
     TreeNode* root = new TreeNode(std::stoi(datas.at(i++)));
-    subDeserialize(datas, i, root);
+    sub_preorder_deserialize_tree_node(datas, i, root);
     return root;
 }
 
-static void subSerialize(TreeNode* root, std::string& ans) {
+static void sub_preorder_serialize_tree_node(TreeNode* root, std::vector<std::string>& ans) {
     if (!root) {
-        ans.append("null,");
+        ans.push_back("null");
         return;
     }
-    ans.append(std::to_string(root->val) + ",");
+    ans.push_back(std::to_string(root->val));
 
-    subSerialize(root->left, ans);
-    subSerialize(root->right, ans);
+    sub_preorder_serialize_tree_node(root->left, ans);
+    sub_preorder_serialize_tree_node(root->right, ans);
 }
 
-std::string LeetcodeUtil::serialize_tree_node(TreeNode* root) {
-    std::string ans;
-    subSerialize(root, ans);
+std::string LeetcodeUtil::preorder_serialize_tree_node(TreeNode* root, bool zip) {
+    std::vector<std::string> words;
+    sub_preorder_serialize_tree_node(root, words);
+
+    if (words.empty()) {
+        return "null";
+    }
+
+    if (zip) {
+        int r = words.size() - 1;
+        while (words[r--] == "null") {
+            words.pop_back();
+        }
+    }
+
+    std::string ans = words[0];
+
+    for (int i = 1; i < words.size(); ++i) {
+        ans.append(",").append(words[i]);
+    }
 
     return ans;
 }
+
+TreeNode* LeetcodeUtil::level_order_deserialize_tree_node(std::string data) {
+    if (data.empty()) return nullptr;
+    std::string str;
+    std::vector<std::string> words = LeetcodeUtil::string_split(data, ',');
+    int j = 0;
+
+    std::queue<TreeNode*> queue;
+    auto root = new TreeNode(std::stoi(words[j++]));
+    queue.emplace(root);
+
+    while (!queue.empty() && j < words.size()) {
+        int size = queue.size();
+        for (int i = 0; i < size && j < words.size(); ++i) {
+            TreeNode* node = queue.front();
+            queue.pop();
+
+            if (node == nullptr) {
+                j += 2;
+            }
+
+            else {
+                if (words[j] != "null") {
+                    auto left_child = new TreeNode(std::stoi(words[j]));
+                    node->left = left_child;
+                    queue.emplace(left_child);
+                }
+                else {
+                    queue.emplace(nullptr);
+                }
+                ++j;
+                if (words[j] != "null") {
+                    auto right_child = new TreeNode(std::stoi(words[j]));
+                    node->right = right_child;
+                    queue.emplace(right_child);
+                }
+                else {
+                    queue.emplace(nullptr);
+                }
+                ++j;
+            }
+        }
+    }
+    return root;
+}
+
+std::string LeetcodeUtil::level_order_serialize_tree_node(TreeNode* root, bool zip) {
+    if (root == nullptr) return "null";
+
+    std::vector<std::string> words;
+    std::queue<TreeNode*> queue;
+    queue.emplace(root);
+
+    while (!queue.empty()) {
+        int size = queue.size();
+        for (int i = 0; i < size; ++i) {
+            TreeNode* node = queue.front();
+            queue.pop();
+            if (node == nullptr) {
+                words.emplace_back("null");
+                queue.emplace(nullptr);
+                queue.emplace(nullptr);
+
+            }
+            else {
+                words.emplace_back(std::to_string(node->val));
+                queue.emplace(node->left);
+                queue.emplace(node->right);
+            }
+        }
+    }
+
+    if (zip) {
+        int r = words.size() - 1;
+        while (words[r--] == "null") {
+            words.pop_back();
+        }
+    }
+
+    std::string ans = words[0];
+
+    for (int i = 1; i < words.size(); ++i) {
+        ans.append(",").append(words[i]);
+    }
+
+    return ans;
+}
+
+#pragma endregion
+
+#pragma endregion
+
+#pragma region String
+
+std::vector<std::string> LeetcodeUtil::string_split(const std::string& str, char delim) {
+    std::size_t previous = 0;
+    std::size_t current = str.find_first_of(delim);
+    std::vector<std::string> elems;
+    while (current != std::string::npos) {
+        if (current > previous) {
+            elems.push_back(str.substr(previous, current - previous));
+        }
+        previous = current + 1;
+        current = str.find_first_of(delim, previous);
+    }
+    if (previous != str.size()) {
+        elems.push_back(str.substr(previous));
+    }
+    return elems;
+}
+
+#pragma endregion
--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.h
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/LeetcodeUtil.h
@@ -6,7 +6,70 @@
 
 class LeetcodeUtil {
 public:
+    /**
+     * \brief 将字符串反序列化为二维 int 数组
+     * \param in 字符串
+     * \return 二维 int 数组 vector<vector<int>>
+     */
     static std::vector<std::vector<int>> deserialize_vector_vector(std::string in);
-    static TreeNode* deserialize_tree_node(std::string data);
-    static std::string serialize_tree_node(TreeNode* root);
+
+    /**
+     * \brief 将字符串反序列化为二叉树，使用先序遍历顺序，且叶子节点的左右子节点为 nullptr\n
+     *  ....1.....                                                \n
+     *  ../...\...                                                \n
+     *  .2.....3.. <==> "1,2,null,null,3,4,null,null,5,null,null" \n
+     *  ....../.\.                                                \n
+     *  .....4...5                                                \n
+     * \param data 字符串
+     * \return 二叉树根节点
+     */
+    static TreeNode* preorder_deserialize_tree_node(std::string data);
+
+    /**
+     * \brief 将二叉树序列化为字符串，使用先序遍历顺序，且叶子节点的左右子节点为 nullptr\n
+     *  ....1.....                                                \n
+     *  ../...\...                                                \n
+     *  .2.....3.. <==> "1,2,null,null,3,4,null,null,5,null,null" \n
+     *  ....../.\.                                                \n
+     *  .....4...5                                                \n
+     * \param root 二叉树根节点
+     * \param zip 是否去除末尾的 null
+     * \return 字符串
+     */
+    static std::string preorder_serialize_tree_node(TreeNode* root, bool zip = false);
+
+    /**
+     * \brief 将字符串反序列化为二叉树，使用层序遍历顺序 \n
+     *  ....1.....                              \n
+     *  ../...\...                              \n
+     *  .2.....3.. <==>  "1,2,3,null,null,4,5"  \n
+     *  ....../.\.                              \n
+     *  .....4...5                              \n
+     * \param data 字符串
+     * \return 二叉树根节点
+     */
+    static TreeNode* level_order_deserialize_tree_node(std::string data);
+
+    /**
+     * \brief 将二叉树序列化为字符串，使用层序遍历顺序\n
+     *  ....1.....                              \n
+     *  ../...\...                              \n
+     *  .2.....3.. <==>  "1,2,3,null,null,4,5"  \n
+     *  ....../.\.                              \n
+     *  .....4...5                              \n
+     * \param root 二叉树根节点
+     * \param zip 是否去除末尾的 null
+     * \return 字符串
+     */
+    static std::string level_order_serialize_tree_node(TreeNode* root, bool zip = false);
+
+
+    /**
+     * \brief 将输入字符串按分隔符分隔为集合
+     * \param str 输入字符串
+     * \param delim 分隔符
+     * \return 分隔后的字符串集合
+     */
+    static std::vector<std::string> string_split(const std::string& str, char delim);
+
 };

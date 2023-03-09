c1ee23befc5e3174289c166f4eaec311569ce453
Lviat Yi <LviatYi@foxmail.com>
2023-03-09 10:17:19 +0800

    feat: add key of Swordii50

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii50.cpp
@@ -0,0 +1,9 @@
+﻿#include "Swordii50.h"
+
+int Swordii50::pathSum(TreeNode* root, int targetSum) {
+    if (!root) {
+        return 0;
+    }
+    return (targetSum == root->val) + pathSum(root->left, targetSum - root->val) + pathSum(
+        root->right, targetSum - root->val) + pathSum(root->left, targetSum) + pathSum(root->right, targetSum);
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii50.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "TreeNode.h"
+
+class Swordii50 {
+public:
+    int pathSum(TreeNode* root, int targetSum);
+};

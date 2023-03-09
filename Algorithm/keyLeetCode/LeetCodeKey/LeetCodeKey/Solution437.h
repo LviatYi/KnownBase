4730d539390754f073144de6805ff99697180e72
Lviat Yi <LviatYi@foxmail.com>
2023-03-09 15:12:11 +0800

    feat: add key of Solution437

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution437.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <unordered_map>
+
+#include "TreeNode.h"
+
+class Solution437 {
+public:
+    std::unordered_map<long long, int> prefix;
+
+    int dfs(TreeNode* root, long long curr, int targetSum);
+
+    int pathSum(TreeNode* root, int targetSum);
+};

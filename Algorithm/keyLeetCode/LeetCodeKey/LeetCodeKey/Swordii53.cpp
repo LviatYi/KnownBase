53fbd8c2263d633288c615416d27fb4a072a867a
Lviat Yi <LviatYi@foxmail.com>
2023-03-13 12:55:32 +0800

    feat: update key of Swordii53

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii53.cpp
@@ -0,0 +1,27 @@
+﻿#include "Swordii53.h"
+
+TreeNode* Swordii53::inorderSuccessor(TreeNode* root, TreeNode* p) {
+    TreeNode* current;
+
+    if (p->right) {
+        current = p->right;
+        while (current->left) {
+            current = current->left;
+        }
+        return current;
+    }
+
+    TreeNode* last_current = nullptr;
+    current = root;
+    while (p->val != current->val) {
+        if (p->val < current->val) {
+            last_current = current;
+            current = current->left;
+        }
+        else {
+            current = current->right;
+        }
+    }
+
+    return last_current;
+}

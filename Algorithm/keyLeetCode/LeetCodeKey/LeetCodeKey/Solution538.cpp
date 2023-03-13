25e03f1ded16836ea8a30972e1705ea65f6bd028
Lviat Yi <LviatYi@foxmail.com>
2023-03-13 12:56:05 +0800

    feat: update key of Solution538

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution538.cpp
@@ -0,0 +1,30 @@
+﻿#include "Solution538.h"
+
+#include <stack>
+
+TreeNode* Solution538::convertBST(TreeNode* root) {
+    if (!root) {
+        return root;
+    }
+    int sum = 0;
+
+    std::stack<TreeNode*> helper_stack;
+
+    TreeNode* current = root;
+
+    while (current || !helper_stack.empty()) {
+        while (current != nullptr) {
+            helper_stack.push(current);
+            current = current->right;
+        }
+
+        current = helper_stack.top();
+        helper_stack.pop();
+
+        sum += current->val;
+        current->val = sum;
+        current = current->left;
+    }
+
+    return root;
+}

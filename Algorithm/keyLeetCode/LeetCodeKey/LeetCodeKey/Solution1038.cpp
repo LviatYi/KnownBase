29f3b121a8d27612e0de3fe33d12c6701ee43090
Lviat Yi <LviatYi@foxmail.com>
2023-03-13 12:55:47 +0800

    feat: update key of Solution1038

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution1038.cpp
@@ -0,0 +1,30 @@
+﻿#include "Solution1038.h"
+
+#include <stack>
+
+TreeNode* Solution1038::bstToGst(TreeNode* root) {
+    if (!root) {
+        return root;
+    }
+    int sum = 0;
+
+    std::stack<TreeNode*> helper_stack;
+
+    TreeNode* current = root;
+
+    while (current || !helper_stack.empty()) {
+        while (current != nullptr) {
+            helper_stack.push(current);
+            current = current->right;
+        }
+
+        current = helper_stack.top();
+        helper_stack.pop();
+
+        sum += current->val;
+        current->val = sum;
+        current = current->left;
+    }
+
+    return root;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution1038.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "TreeNode.h"
+
+class Solution1038 {
+public:
+    TreeNode* bstToGst(TreeNode* root);
+};

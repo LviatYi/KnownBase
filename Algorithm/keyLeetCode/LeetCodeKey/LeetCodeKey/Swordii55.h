07062ac1a9b53f3cf0ba1da016a5d7ed6d207bfe
Lviat Yi <LviatYi@foxmail.com>
2023-03-13 13:14:37 +0800

    feat: update key of Swordii55

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii55.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <stack>
+
+#include "TreeNode.h"
+
+class Swordii55 {
+private:
+    std::stack<TreeNode*> stack_;
+public:
+    Swordii55(TreeNode* root);
+
+    int next();
+
+    bool hasNext();
+};

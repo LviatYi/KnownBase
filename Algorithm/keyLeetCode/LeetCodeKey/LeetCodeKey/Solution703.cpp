25ac960b6e1c909da03f2004343020f92e87a9f0
Lviat Yi <LviatYi@foxmail.com>
2023-03-14 15:24:06 +0800

    feat: update key of Solution703

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution703.cpp
@@ -0,0 +1,23 @@
+﻿#include "Solution703.h"
+
+Solution703::Solution703(int k, std::vector<int>& nums) {
+    k_ = k;
+    if (nums.empty()) {
+        return;
+    }
+
+    for (int num : nums) {
+        add(num);
+    }
+}
+
+int Solution703::add(int val) {
+    if (front_.size() < k_) {
+        front_.push(val);
+    }
+    else {
+        front_.push(val);
+        front_.pop();
+    }
+    return front_.top();
+}

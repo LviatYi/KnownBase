c4773efc4efae64939297e5a4f5a442219ae255a
Lviat Yi <LviatYi@foxmail.com>
2023-03-14 15:24:11 +0800

    feat: update key of Solution729

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution729.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <map>
+#include <unordered_set>
+
+class Solution729 {
+private:
+    std::map<int, int> calendar_;
+    /// <summary>
+    /// tree_ 表示区间 [l,r] 是否存在标记为 1 的元素
+    /// lazy_ 表示区间 [l,r] 全为标记为 1 的元素
+    /// </summary>
+    std::unordered_set<int> tree_, lazy_;
+
+    bool query(int start, int end, int l, int r, int idx);
+
+    void update(int start, int end, int l, int r, int idx);
+
+public:
+    Solution729();
+
+    bool book(int start, int end);
+
+    bool book2(int start, int end);
+};

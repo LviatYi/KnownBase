2880737f198d1434c143fdc410536474d964f12a
Lviat Yi <LviatYi@foxmail.com>
2023-03-17 14:46:20 +0800

    feat: update key of Solution373

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution373.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <queue>
+#include <utility>
+#include <vector>
+
+class Solution373 {
+public:
+    std::vector<std::vector<int>> kSmallestPairs(std::vector<int>& nums1, std::vector<int>& nums2, int k);
+};

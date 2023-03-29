0a1ccfcfceb86fbed7735e48ccbf4cd951dca24b
Lviat Yi <LviatYi@foxmail.com>
2023-03-29 13:35:17 +0800

    feat: update key of Solution2469

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution2469.cpp
@@ -0,0 +1,10 @@
+﻿#include "Solution2469.h"
+
+std::vector<double> Solution2469::convertTemperature(double celsius) {
+    std::vector<double> ret;
+
+    ret.push_back(celsius + 273.15);
+    ret.push_back(celsius * 1.8 + 32.0);
+
+    return ret;
+}

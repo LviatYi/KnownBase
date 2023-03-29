b7fbd41aada426107272e16e9cf1deefa1ea77e0
Lviat Yi <LviatYi@foxmail.com>
2023-03-29 13:35:27 +0800

    feat: update key of Swordii64

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii64.cpp
@@ -0,0 +1,49 @@
+﻿#include "Swordii64.h"
+
+bool Swordii64::sub_search(std::string& searchWord, size_t index, Swordii64::character_dict* current, int tolerance) {
+    if (tolerance < 0) {
+        return false;
+    }
+
+    if (index >= searchWord.size()) {
+        return tolerance == 0 && current->is_end;
+    }
+
+    for (int i = 0; i < character_dict::size - 1; ++i) {
+        if (current->chars[i] == nullptr) {
+            continue;
+        }
+
+        if (i == searchWord[index] - 'a' && current->chars[i] != nullptr && sub_search(
+                searchWord, index + 1, current->chars[i], tolerance)
+            || i != searchWord[index] - 'a' && current->chars[i] != nullptr && sub_search(
+                searchWord, index + 1, current->chars[i], tolerance - 1)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+Swordii64::Swordii64() {
+    dict = new character_dict();
+}
+
+void Swordii64::buildDict(std::vector<std::string> dictionary) {
+    for (auto string : dictionary) {
+        character_dict* current = dict;
+
+        for (const char c : string) {
+            if (current->chars[c - 'a'] == nullptr) {
+                current->chars[c - 'a'] = new character_dict;
+            }
+            current = current->chars[c - 'a'];
+        }
+
+        current->is_end = true;
+    }
+}
+
+bool Swordii64::search(std::string searchWord) {
+    return sub_search(searchWord, 0, dict, 1);
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii64.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class Swordii64 {
+private:
+    struct character_dict {
+        static constexpr int size = 26;
+
+        character_dict* chars[size] = {nullptr};
+        bool is_end = false;
+    };
+
+    character_dict* dict;
+
+    bool sub_search(std::string& searchWord, size_t index, character_dict* current, int tolerance);
+
+public:
+    /** Initialize your data structure here. */
+    Swordii64();
+
+    void buildDict(std::vector<std::string> dictionary);
+
+    bool search(std::string searchWord);
+};

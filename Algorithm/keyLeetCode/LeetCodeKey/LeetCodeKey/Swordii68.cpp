a4494b94510b5920cb1dfd5b1fe89bd593e2484b
Lviat Yi <LviatYi@foxmail.com>
2023-04-03 23:54:23 +0800

    feat: update key of Swordii68

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii68.cpp
@@ -0,0 +1,26 @@
+﻿#include "Swordii68.h"
+
+int Swordii68::searchInsert(std::vector<int>& nums, int target) {
+    if (nums.empty()) {
+        return 0;
+    }
+
+    size_t start = 0;
+    size_t end = nums.size();
+    while (start < end) {
+        size_t i = (start + end) / 2;
+        if (nums[i] == target) {
+            return static_cast<int>(i);
+        }
+
+        if (nums[i] > target) {
+            end = i;
+        }
+        else {
+            start = i + 1;
+        }
+
+    }
+
+    return start;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii68.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include <vector>
+
+class Swordii68 {
+public:
+    int searchInsert(std::vector<int>& nums, int target);
+};

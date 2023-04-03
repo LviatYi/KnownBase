32470a2230b2f9a008f7a8596a7e8c9b5f12d403
Lviat Yi <LviatYi@foxmail.com>
2023-04-03 23:53:49 +0800

    feat: update key of Solution677

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution677.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <string>
+
+class Solution677 {
+private:
+    struct Character_dict {
+        static constexpr int size = 26;
+
+        Character_dict* chars[size] = {nullptr};
+        int val = 0;
+
+        void insert(std::string& word, const int& end_val, int index = 0);
+    };
+
+    Character_dict* dict_;
+public:
+    Solution677();
+
+    void insert(std::string key, int val);
+
+    int sum(std::string prefix);
+};

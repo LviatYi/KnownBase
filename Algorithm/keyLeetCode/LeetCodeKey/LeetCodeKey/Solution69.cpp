f002d5b78278abeb33fb00a1b7f8088d50716486
Lviat Yi <LviatYi@foxmail.com>
2023-04-04 17:08:47 +0800

    feat: update key of Solution69

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution69.cpp
@@ -0,0 +1,16 @@
+﻿#include "Solution69.h"
+
+int Solution69::mySqrt(int x) {
+    int l = 0, r = x, ans = -1;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (static_cast<long long>(mid) * mid <= x) {
+            ans = mid;
+            l = mid + 1;
+        }
+        else {
+            r = mid - 1;
+        }
+    }
+    return ans;
+}

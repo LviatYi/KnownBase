831774569905097c021f1dc51481601549da6bd0
Lviat Yi <LviatYi@foxmail.com>
2023-04-04 17:06:02 +0800

    feat: update key of Solution875

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution875.cpp
@@ -0,0 +1,26 @@
+﻿#include "Solution875.h"
+
+#include <algorithm>
+
+int Solution875::minEatingSpeed(std::vector<int>& piles, int h) {
+    int begin = 1;
+    int end = *std::max_element(piles.begin(), piles.end());
+
+    while (begin < end) {
+        int size = (begin + end) >> 1;
+        int pile_count = 0;
+        for (int pile : piles) {
+            pile_count += pile / size;
+            pile_count += (pile % size) > 0;
+            if (pile_count > h) {
+                begin = size + 1;
+                break;
+            }
+        }
+        if (pile_count <= h) {
+            end = size;
+        }
+    }
+
+    return end;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution875.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include <vector>
+
+class Solution875 {
+public:
+    int minEatingSpeed(std::vector<int>& piles, int h);
+};

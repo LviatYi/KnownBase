e536312767183bfd7b949d4f05736219f7676d45
Lviat Yi <LviatYi@foxmail.com>
2023-04-06 13:01:07 +0800

    feat: update key of Solution56

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution56.cpp
@@ -0,0 +1,28 @@
+﻿#include "Solution56.h"
+
+#include <algorithm>
+
+std::vector<std::vector<int>> Solution56::merge(std::vector<std::vector<int>>& intervals) {
+    std::sort(intervals.begin(), intervals.end(),
+              [](const std::vector<int>& lhs, const std::vector<int>& rhs)-> bool {
+                  return lhs[0] < rhs[0] || lhs[0] == rhs[0] && lhs[1] < rhs[1];
+              });
+
+    std::vector<std::vector<int>> ret = {};
+
+    int index = 0;
+    while (index < intervals.size()) {
+        int start = intervals[index][0];
+        int end = intervals[index][1];
+        int p = index + 1;
+
+        while (p < intervals.size() && intervals[p][0] <= end) {
+            end = std::max(intervals[p++][1], end);
+        }
+
+        ret.push_back(std::vector<int>{start, end});
+        index = p;
+    }
+
+    return ret;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution56.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include <vector>
+
+class Solution56 {
+public:
+    std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals);
+};

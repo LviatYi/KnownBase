a292ba7ad7ca1b262705e11e6a318b2b8b575af4
Lviat Yi <LviatYi@foxmail.com>
2023-04-06 13:33:38 +0800

    feat: update key of Solution148

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution148.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "ListNode.h"
+
+class Solution148 {
+public:
+    ListNode* merge(ListNode* head1, ListNode* head2);
+    ListNode* sortList(ListNode* head);
+    ListNode* sortList(ListNode* head, ListNode* tail);
+
+    ListNode* sortList2(ListNode* head);
+};

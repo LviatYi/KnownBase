041214c62a6fb481dae354245dcb89e7af5b72bd
Lviat Yi <LviatYi@foxmail.com>
2023-04-06 13:03:32 +0800

    feat: update key of Swordii75

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii75.cpp
@@ -0,0 +1,22 @@
+﻿#include "Swordii75.h"
+
+#include <algorithm>
+
+std::vector<int> Swordii75::relativeSortArray(std::vector<int>& arr1, std::vector<int>& arr2) {
+    const int size = 1001;
+    int map[size];
+    std::fill(std::begin(map), std::end(map), INT_MAX);
+
+    for (int i = 0; i < arr2.size(); ++i) {
+        map[arr2[i]] = i;
+    }
+
+    std::sort(arr1.begin(), arr1.end(), [map](const int lhs, const int rhs)-> bool {
+        const int val_lhs = map[lhs];
+        const int val_rhs = map[rhs];
+
+        return val_lhs == INT_MAX && val_rhs == INT_MAX ? lhs < rhs : val_lhs < val_rhs;
+    });
+
+    return arr1;
+}
---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii75.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include <vector>
+
+class Swordii75 {
+public:
+    std::vector<int> relativeSortArray(std::vector<int>& arr1, std::vector<int>& arr2);
+};

c2fc2859268f70f38ddb6c05f175c054bae5115b
Lviat Yi <LviatYi@foxmail.com>
2023-04-06 13:03:45 +0800

    feat: update Leetcode keys

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Main.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Main.cpp
@@ -18,16 +18,19 @@ int main() {
     //auto input = LeetcodeUtil::deserialize_vector_vector(in);
 
     int val = 2;
-    vector<int> vector = {2, 2};
+    vector<int> vector_int = {3, 2, 1, 5, 6, 4};
     std::vector<string> strings = {"time", "me", "bell"};
     TreeNode* tree = LeetcodeUtil::level_order_deserialize_tree_node("8,6,10,5,7,9,11");
+    std::vector<std::vector<int>> vector_vector_int = LeetcodeUtil::deserialize_vector_vector(
+        "[[1,3],[1,2],[8,10],[15,18],[2,6]]");
 
-    Swordii73 solution;
+    Swordii76 solution;
     //Swordii73 solution(vector);
 
     //auto ret = solution.book();
 
-    auto ret = solution.minEatingSpeed(vector, val);
+    auto ret = solution.findKthLargest2(vector_int, 4);
+
     //Node* head = new Node(1, new Node(2, new Node(3, new Node(4))));
 
     //auto ans = solution.reorderList2(head);

2bf76c049902ed8618e5e771afbe717826ae64cc
Lviat Yi <LviatYi@foxmail.com>
2023-04-07 23:47:05 +0800

    feat: update key of Swordii81

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii81.cpp
@@ -0,0 +1,22 @@
+﻿#include "Swordii81.h"
+
+void Swordii81::dfs(std::vector<int>& candidates, int target, int min_index) {
+    if (target < 0) {
+        return;
+    }
+    if (target == 0) {
+        ret.push_back(t);
+        return;
+    }
+
+    for (int i = min_index; i < candidates.size(); ++i) {
+        t.push_back(candidates[i]);
+        dfs(candidates, target - candidates[i], i);
+        t.pop_back();
+    }
+}
+
+std::vector<std::vector<int>> Swordii81::combinationSum(std::vector<int>& candidates, int target) {
+    dfs(candidates, target, 0);
+    return ret;
+}

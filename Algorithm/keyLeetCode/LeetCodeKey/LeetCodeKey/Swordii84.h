2b969a1916e379d8bc4ff12a0971022ea84c847a
Lviat Yi <LviatYi@foxmail.com>
2023-04-10 14:31:40 +0800

    feat: update key of Swordii84

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Swordii84.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <vector>
+
+class Swordii84 {
+private:
+    std::vector<std::vector<int>> ans;
+    std::vector<int> temp;
+    void dfs(std::vector<int>& nums, int* is_chosen);
+
+public:
+    std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums);
+};

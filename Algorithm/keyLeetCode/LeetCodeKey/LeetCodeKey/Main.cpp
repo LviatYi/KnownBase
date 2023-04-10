70197e79968285647229cb9c5c4165c573546bf2
Lviat Yi <LviatYi@foxmail.com>
2023-04-10 14:32:17 +0800

    feat: update Leetcode keys

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Main.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Main.cpp
@@ -4,6 +4,7 @@
 #include "ListNode.h"
 #include "TreeNode.h"
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 #include <queue>
@@ -14,7 +15,7 @@ using namespace std;
 
 int main() {
     int val = 2;
-    vector<int> int_list = {3, 2, 1, 5, 6, 4};
+    vector<int> int_list = {1, 1, 2};
     std::vector<std::vector<int>> int_list_list = LeetcodeUtil::deserialize_vector_vector(
         "[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]");
 
@@ -25,12 +26,12 @@ int main() {
     ListNode* list_node = LeetcodeUtil::deserialize_list_node("[1,4,5]");
     vector<ListNode*> list_node_list = LeetcodeUtil::deserialize_vector_list_node("[[1,4,5], [1,3,4], [2,6]]");
 
-    Swordii78 solution;
+    Swordii84 solution;
     //Swordii73 solution(vector);
 
     //auto ret = solution.book();
 
-    auto ret = solution.mergeKLists(list_node_list);
+    auto ret = solution.permuteUnique(int_list);
 
     //Node* head = new Node(1, new Node(2, new Node(3, new Node(4))));
 

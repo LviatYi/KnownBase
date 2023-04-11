dfc69aa9ec3f403b5ce6292e3e8e218ad09e1ffb
Lviat Yi <LviatYi@foxmail.com>
2023-04-11 13:57:18 +0800

    feat: update Leetcode keys

--- Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Main.cpp
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Main.cpp
@@ -19,6 +19,7 @@ int main() {
     std::vector<std::vector<int>> int_list_list = LeetcodeUtil::deserialize_vector_vector(
         "[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]");
 
+    std::string str = "google";
     std::vector<string> strings = {"time", "me", "bell"};
 
     TreeNode* tree = LeetcodeUtil::level_order_deserialize_tree_node("8,6,10,5,7,9,11");
@@ -26,12 +27,12 @@ int main() {
     ListNode* list_node = LeetcodeUtil::deserialize_list_node("[1,4,5]");
     vector<ListNode*> list_node_list = LeetcodeUtil::deserialize_vector_list_node("[[1,4,5], [1,3,4], [2,6]]");
 
-    Swordii84 solution;
+    Swordii86 solution;
     //Swordii73 solution(vector);
 
     //auto ret = solution.book();
 
-    auto ret = solution.permuteUnique(int_list);
+    auto ret = solution.partition("google");
 
     //Node* head = new Node(1, new Node(2, new Node(3, new Node(4))));
 

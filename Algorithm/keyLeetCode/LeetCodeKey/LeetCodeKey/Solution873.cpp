419047c119b309631a4fd94662a67314bbc429f3
Lviat Yi <LviatYi@foxmail.com>
2023-04-13 12:59:18 +0800

    feat: update key of Solution873

---
+++ Algorithm/keyLeetCode/LeetCodeKey/LeetCodeKey/Solution873.cpp
@@ -0,0 +1,27 @@
+﻿#include "Solution873.h"
+
+#include <unordered_map>
+
+int Solution873::lenLongestFibSubseq(std::vector<int>& arr) {
+    std::unordered_map<int, int> map;
+    size_t n = arr.size();
+    std::vector dp(n, std::vector(n, 0));
+
+
+    for (int i = 0; i < n; ++i) {
+        map[arr[i]] = i;
+    }
+
+    int max = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] - arr[i] < arr[i] && map.count(arr[j] - arr[i])) {
+                int current = dp[map[arr[j] - arr[i]]][i] + 1;
+                max = std::max(current, max);
+                dp[i][j] = current;
+            }
+        }
+    }
+
+    return max != 0 ? max + 2 : 0;
+}
